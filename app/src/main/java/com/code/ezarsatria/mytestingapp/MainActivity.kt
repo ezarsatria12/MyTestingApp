713a3cc882ff064e1c9c2b0bc4bb4e5926093824
Ezarsatria12 <Ezar3004>
2020-12-13 21:50:06 +0700

    assignment

---
+++ app/src/main/java/com/code/ezarsatria/mytestingapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.code.ezarsatria.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+    private lateinit var imgPreview: ImageView
+
+    private var names = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSetValue = findViewById(R.id.btn_set_value)
+        tvText = findViewById(R.id.tv_text)
+
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Ezar Satria")
+        names.add("Adolf Hitler")
+        names.add("Joseph Stalin")
+
+        imgPreview = findViewById(R.id.img_preview)
+        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
